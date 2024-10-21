f97d71a5da93edae925e1a439777e17437d62b0d
Juya <juyaamirm@gmail.com>
2024-10-21 17:36:21 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <Router>  
+  <Router basename='/IT-Job-Nest'>  
     <StrictMode>
       <App />
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/It-Job-Nest/',
+  base: '/IT-Job-Nest/',
   plugins: [react()],
 })
