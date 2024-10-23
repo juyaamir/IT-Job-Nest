91b59b8041b1a32d2a25259466b472b25685c14b
Juya <juyaamirm@gmail.com>
2024-10-23 13:04:36 +0200

    deploy

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,8 +1,14 @@
 import Logo from './Logo'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { MdOutlineHomeWork } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
 const MobileNavbar = () => {
-  const [isOpen, setIsOpen] = useState<Boolean>(false)
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const handleClick = () => {
+    setIsOpen(false)
+  }
   return (
     <div className='flex justify-between p-2 '>
       <Link to='/'>
@@ -11,12 +17,12 @@ const MobileNavbar = () => {
       <button className='text-3xl ' onClick={()=> setIsOpen(true)}>🍔</button>
       {
         isOpen && (
-      <div className='inset-0 bg-black absolute z-10 bg-opacity-50'>
-        <div className="flex flex-col gap-4 bg-white fixed top-0 bottom-1/2 left-0 right-1/2 p-3 shadow-lg rounded-md">
-            <button className='fixed right-2 text-2xl bg-white py-2 px-4 rounded-full ' onClick={()=> setIsOpen(false)}>X</button>
-            <Link to='/companies'>Companies</Link>
-            <Link to='/favorites'>Favorites</Link>
-            <Link to='/login'>Login</Link>
+      <div className='fixed inset-0 bg-black z-10 bg-opacity-50 '>
+        <div className="absolute flex flex-col gap-4 bg-white  inset-x-1/4 inset-y-1/3 p-3 shadow-lg rounded-md">
+            <button className='fixed right-2  top-2 text-2xl bg-white py-2 px-4 rounded-full hover:text-red-500' onClick={handleClick}>X</button>
+            <Link to='/companies' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><MdOutlineHomeWork /> Companies</Link>
+            <Link to='/favorites' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><MdFavoriteBorder />Favorites</Link>
+            <Link to='/login' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><FaRegUserCircle />Login</Link>
         </div>
       </div>
         )
