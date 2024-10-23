e7101e4bd918a41f538833c5f175a9b9be43b8d0
Juya <juyaamirm@gmail.com>
2024-10-23 14:14:50 +0200

    deploy

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom"
 import Logo from './Logo'
-
+import { MdOutlineHomeWork } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 const DesktopNav = () => {
 
   return (
     <div className="flex flex-wrap justify-between p-2">
         <Link to='/'><Logo />
         </Link>
-        <ul className="flex flex-wrap justify-between gap-4">
-            <li><Link to='/companies'>Companies</Link></li>
-            <li><Link to='/favorites'>Favorites</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-          {/*   <li><Link to='/theme'>Theme</Link></li> */}
-        </ul>
+        <div className="flex flex-wrap justify-between gap-6">
+            <Link to='/companies' className="flex items-center gap-2 lg:text-2xl"><FaSearch />Search Job</Link>
+            <Link to='/companies' className="flex items-center gap-2 lg:text-2xl"><MdOutlineHomeWork />Companies</Link>
+            <Link to='/favorites' className="flex items-center gap-2 lg:text-2xl"><MdFavoriteBorder />Favorites</Link>
+            <Link to='/login' className="flex items-center gap-2 lg:text-2xl"><FaRegUserCircle/>Login</Link>
+          {/*   <Link to='/theme'>Theme</Link> */}
+        </div>
     </div>
   )
 }
--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { MdOutlineHomeWork } from "react-icons/md";
 import { MdFavoriteBorder } from "react-icons/md";
 import { FaRegUserCircle } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 const MobileNavbar = () => {
   const [isOpen, setIsOpen] = useState<Boolean>(false);
   const handleClick = () => {
@@ -18,11 +19,15 @@ const MobileNavbar = () => {
       {
         isOpen && (
       <div className='fixed inset-0 bg-black z-10 bg-opacity-50 '>
-        <div className="absolute flex flex-col gap-4 bg-white  inset-x-1/4 inset-y-1/3 p-3 shadow-lg rounded-md">
-            <button className='fixed right-2  top-2 text-2xl bg-white py-2 px-4 rounded-full hover:text-red-500' onClick={handleClick}>X</button>
-            <Link to='/companies' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><MdOutlineHomeWork /> Companies</Link>
-            <Link to='/favorites' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><MdFavoriteBorder />Favorites</Link>
-            <Link to='/login' onClick={handleClick} className='flex  items-center gap-2 text-xl p-2 hover:bg-gray-200 rounded-md'><FaRegUserCircle />Login</Link>
+        <div className="absolute flex flex-col  bg-white  inset-x-0 top-0 bottom-0 p-3 shadow-lg ">
+          <div className='flex justify-between mb-4'>
+            <Link to='/' onClick={handleClick}><Logo /></Link>
+            <button className='fixed right-2  top-2 text-2xl bg-white py-2 px-4 rounded-full hover:text-red-500 ' onClick={handleClick}>X</button>
+          </div>
+          <Link to='/' onClick={handleClick} className='flex  items-center gap-2 text-xl  px-2 py-3 hover:bg-gray-200  border-y border-y-gray-300'><FaSearch /> Search Job</Link>
+            <Link to='/companies' onClick={handleClick} className='flex  items-center gap-2 text-xl  px-2 py-3 hover:bg-gray-200  border-y border-y-gray-300'><MdOutlineHomeWork /> Companies</Link>
+            <Link to='/favorites' onClick={handleClick} className='flex  items-center gap-2 text-xl  px-2 py-3 hover:bg-gray-200 '><MdFavoriteBorder />Favorites</Link>
+            <Link to='/login' onClick={handleClick} className='flex  items-center gap-2 text-xl  px-2 py-3 hover:bg-gray-200 border-y border-y-gray-300' ><FaRegUserCircle />Login</Link>
         </div>
       </div>
         )
