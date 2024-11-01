0d146b2e2c8559749d4ce8bb7b508f1e263301d1
Juya <juyaamirm@gmail.com>
2024-11-01 22:12:17 +0100

    dashboard and profile is added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PiSignOutBold } from "react-icons/pi";
+import { useAuth } from '../context/AuthContext';
+import axios from 'axios';
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+interface ProfileData {
+  name: string;
+  email: string;
+  // Add other profile fields as needed
 }
 
-export default Dashboard
+const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useAuth();
+  const [error, setError] = useState<string>('');
+  const [myProfile, setMyProfile] = useState<ProfileData | null>(null);
+
+  const handleSignOut = () => {
+    navigate('/');
+    setIsAuthenticated(false);
+    localStorage.removeItem('token');
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setError('You are not authorized to view this page');
+      setTimeout(() => {
+        navigate('/login');
+      }, 2000);
+      return;
+    }
+
+    const getProfile = async () => {
+      try {
+        const res = await axios.get('http://localhost:8000/users/v1/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log('Profile: ', res.data);
+        setMyProfile(res.data.profile);
+      } catch (error) {
+        console.error('Error: ', error);
+        setError('An error occurred while trying to get your profile');
+      }
+    };
+
+    getProfile();
+  }, [navigate]);
+
+  if (error) {
+    return <p className='text-red-500 text-center'>{error}</p>;
+  }
+
+  if (!myProfile) {
+    return <p className='text-center'>Loading...</p>;
+  }
+
+  return (
+    <div className='text-xl relative group'>
+      <p className='px-4 py-2 hover:cursor-pointer'>Hi, {myProfile.name}</p>
+      <div className='border border-gray-400 w-96 p-6 rounded absolute'>
+        <div className='flex gap-2 p-2 bg-cyan-300 rounded'>
+          <img src='' alt='' className='h-12 w-12 border-blue-400 border rounded-full' />
+          <p className='flex flex-col'>
+            <span className='font-bold text-base'>{myProfile.name}</span>
+            <span className='text-sm'>Thanks for being a JA-TechStore customer</span>
+          </p>
+        </div>
+        <div className='flex justify-between text-base py-4 dashboard-list'>
+          <ul className='font-semibold flex flex-col gap-3'>
+            <li>
+              <button className='flex items-center gap-2' onClick={handleSignOut}>
+                <PiSignOutBold className='text-xl' /> <span>Sign Out</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FiEdit } from "react-icons/fi"
 
 interface ApiData {
   name: string;
@@ -10,6 +11,7 @@ interface ApiData {
 const Profile: React.FC = () => {
   const [profileData, setProfileData] = useState<ApiData | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     // Get token from local storage
@@ -46,15 +48,37 @@ const Profile: React.FC = () => {
 
   return (
     <div className="profile-container">
-      {error && <p className="text-red-400 italic">{error}</p>}
-      {profileData ? (
-        <div>
-          <p>Name: {profileData.name}</p>
-          <p>Email: {profileData.email}</p>
+      <div className='flex gap-8'>
+      <div className='flex flex-col text-center w-44 justify-center items-center p-2'>
+        <img src='' alt="image" className='h-28 w-28 rounded-full ' />
+        <div className='flex gap-2 '>
+          <h4 className='font-bold'> { profileData?.name} </h4>
+          <button onClick={() => setIsEditing(true)} title='Change profile photo' className='text-xl hover:text-orange-400'> <FiEdit /></button>
         </div>
-      ) : (
-        !error && <p>Loading profile data...</p>
-      )}
+        
+      </div>
+      <div className='border border-gray-400 p-2'>
+        <h1 className='font-bold '>Your Profile</h1>
+        <p>Your profile preferences help us personalize recommendations for you</p>
+      </div>
+    </div>
+    {
+      isEditing && (
+        <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 '>
+          <div className='bg-white border border-gray-300 p-4 w-96 shadow-lg rounded-md'>
+            <h1 className='font-bold mb-4'>Edit profile picture</h1>
+          <div>
+            <input type="file" title='upload your profile' />
+          </div>
+          <div className='flex justify-end mt-2'>
+          <button className='bg-blue-500 text-white rounded px-4 py-2 mr-2'>Update</button>
+          <button className='bg-gray-300 px-4 py-2 mr-2 rounded ' onClick={()=> setIsEditing(false)}>Cancel</button>
+            
+          </div>
+          </div>
+        </div>
+      )
+    }
     </div>
   );
 };
