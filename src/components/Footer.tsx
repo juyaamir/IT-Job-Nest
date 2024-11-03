9b80436bea905a1a7672f3a18eda75bef1f21599
Juya <juyaamirm@gmail.com>
2024-11-03 10:50:17 +0100

    footer data was added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,24 @@
 
+import { Link } from 'react-router-dom'
+import { footerLinks } from '../contents/footerData'
 
 const Footer = () => {
   return (
-    <div className="min-h-24 border-t border-gray-400">Footer</div>
+      <footer className=' '>
+        <div className='flex flex-wrap justify-around items-center border border-b-gray-300 p-1'>  
+            <div className='flex gap-4 flex-wrap'> 
+            <p className='flex items-center justify-center font-bold text-gray-600 '>Follow Us</p>
+            {
+              footerLinks.map((item) => (
+                <Link key={item.path} to = {item.path} target='_blank' rel='noreferrer' className='hover:scale-125 duration-500 transition-transform  '>
+                  <img src={item.source} alt={item.alt} className='h-10 w-10' />
+                </Link>
+              ))
+            }
+          </div>
+        </div>
+        <p className='text-sm md:text-lg text-center p-1'>&copy; 2024 | Designed & Coded with ❤️ by Amir Muhammad Juya</p>
+      </footer>
   )
 }
 
